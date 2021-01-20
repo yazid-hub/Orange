ff46a6039c628d76aaf2c51e08784612d8c5f930
yazid-hub <naitallouyazid@yahoo.fr>
2021-01-20 14:09:54 +0100

    dernierver

--- Orange/Bdd.cs
+++ Orange/Bdd.cs
@@ -2,47 +2,51 @@
 using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.Data.Common;
+
+
 namespace Orange
 {
     public class Bdd
     {
         private string serveur, bdd, user, mdp;
         private MySqlConnection maConnexion;
+
         public Bdd(string serveur, string bdd, string user, string mdp)
         {
             this.serveur = serveur;
             this.bdd = bdd;
-            this.user = user; 
+            this.user = user;
             this.mdp = mdp;
-            string url = "SERVER=" + this.serveur + ";port=307" + ";Database=" + this.bdd + ";User Id=" + this.user + ";password=" + this.mdp;
+            string url = "SERVER=" + this.serveur + ";port=3306" + ";Database=" + this.bdd + "; User Id=" + this.user + ";password=" + this.mdp;
+            //attention pour les PC port =3306
 
             try
             {
                 this.maConnexion = new MySqlConnection(url);
-                Console.WriteLine("connexion réuissie");
+                Console.WriteLine("Connexion réussie");
             }
             catch (Exception exp)
             {
-                Console.WriteLine("Erreur de connexion à  : " + url);
+                Console.WriteLine("Erreur de connexion à : " + url);
                 Console.WriteLine(exp.Message);
-
             }
-        }
 
+        }
         public List<Intervention> selectAllInterventions()
         {
             List<Intervention> lesInterventions = new List<Intervention>();
-            string requete = "select * from intervention ;";
+            string requete = "select * from intervention ; ";
+
             try
             {
-                // ouverture de la connexion mysql
+                //ouverture de la connexion Mysql
                 this.maConnexion.Open();
+                //creation de la commande SQL
                 MySqlCommand cmd = this.maConnexion.CreateCommand();
 
                 cmd.CommandText = requete;
 
-                // parcourir les enregistrement de la table
-
+                //parcourir les enregistrements de la table
                 DbDataReader unReader = cmd.ExecuteReader();
                 try
                 {
@@ -55,8 +59,6 @@ namespace Orange
                                 unReader.GetString(5), unReader.GetString(6)
                                 );
                             lesInterventions.Add(uneIntervention);
-
-
                         }
                     }
                 }
@@ -64,29 +66,32 @@ namespace Orange
                 {
                     Console.WriteLine("Erreur d'extraction des données de la table ");
                 }
-                // fermeture de la connexion
+
+                //fermeture de la connexion 
                 this.maConnexion.Close();
             }
             catch (Exception exp)
             {
-                Console.WriteLine("Erreur d'éxécution de la requete: " + requete);
+                Console.WriteLine("Erreur d'exécution de la requete :" + requete);
+                Console.WriteLine(exp.Message);
             }
+
             return lesInterventions;
         }
-
         public void insertIntervention(Orange.Intervention uneIntervention)
         {
-            String requete = "";
-
+            string requete = "";
             try
             {
                 this.maConnexion.Open();
 
-                requete = "insert into intervention values(null,@description, @dateinter,@montant, @statut, @technicien, @client);";
+                requete = "insert into intervention values (null, @description, @dateinter,  @montant , @statut, @technicien, @client );";
 
                 MySqlCommand cmd = this.maConnexion.CreateCommand();
 
-                // correspondance des parametres et des valeurs
+                cmd.CommandText = requete;
+
+                //correspondance des parametres et des valeurs
                 cmd.Parameters.AddWithValue("@description", uneIntervention.DESCRIPTION);
                 cmd.Parameters.AddWithValue("@dateinter", uneIntervention.DATEINTER);
                 cmd.Parameters.AddWithValue("@montant", uneIntervention.MONTANT);
@@ -94,22 +99,22 @@ namespace Orange
                 cmd.Parameters.AddWithValue("@technicien", uneIntervention.TECHNICIEN);
                 cmd.Parameters.AddWithValue("@client", uneIntervention.CLIENT);
 
-                // on execute la requete
+                //on execute la requete
                 cmd.ExecuteNonQuery();
+
                 this.maConnexion.Close();
             }
-
             catch (Exception exp)
             {
-                Console.WriteLine("Erreur d'éxecution de la requete :  " + requete);
+                Console.WriteLine("Erreur d'execution de la requete :" + requete);
+                Console.WriteLine(exp.Message);
+
             }
         }
 
-
         public void deleteIntervention(int idInter)
         {
-            String requete = "";
-
+            string requete = "";
             try
             {
                 this.maConnexion.Open();
@@ -118,36 +123,38 @@ namespace Orange
 
                 MySqlCommand cmd = this.maConnexion.CreateCommand();
 
-                // correspondance des parametres et des valeurs
+                cmd.CommandText = requete;
+
+                //correspondance des parametres et des valeurs
                 cmd.Parameters.AddWithValue("@idinter", idInter);
 
-
-
-                // on execute la requete
+                //on execute la requete
                 cmd.ExecuteNonQuery();
+
                 this.maConnexion.Close();
             }
-
             catch (Exception exp)
             {
-                Console.WriteLine("Erreur d'éxecution de la requete :  " + requete);
+                Console.WriteLine("Erreur d'execution de la requete :" + requete);
+                Console.WriteLine(exp.Message);
             }
         }
 
-
         public void updateIntervention(Orange.Intervention uneIntervention)
         {
-            String requete = "";
-
+            string requete = "";
             try
             {
                 this.maConnexion.Open();
 
-                requete = "update intervention set description = @description,dateinter =@dateinter,montant = @montant, statut= @statut," + "technicien=  @technicien, client = @client wher idinter = @idinter);";
+                requete = "update intervention set description= @description, dateinter= @dateinter, montant= @montant , statut =@statut, " +
+                    " technicien = @technicien, client = @client where idinter = @idinter ;";
 
                 MySqlCommand cmd = this.maConnexion.CreateCommand();
 
-                // correspondance des parametres et des valeurs
+                cmd.CommandText = requete;
+
+                //correspondance des parametres et des valeurs
                 cmd.Parameters.AddWithValue("@description", uneIntervention.DESCRIPTION);
                 cmd.Parameters.AddWithValue("@dateinter", uneIntervention.DATEINTER);
                 cmd.Parameters.AddWithValue("@montant", uneIntervention.MONTANT);
@@ -155,48 +162,43 @@ namespace Orange
                 cmd.Parameters.AddWithValue("@technicien", uneIntervention.TECHNICIEN);
                 cmd.Parameters.AddWithValue("@client", uneIntervention.CLIENT);
                 cmd.Parameters.AddWithValue("@idinter", uneIntervention.iDinter);
-
-
-                // on execute la requete
+                //on execute la requete
                 cmd.ExecuteNonQuery();
+
                 this.maConnexion.Close();
             }
-
             catch (Exception exp)
             {
-                Console.WriteLine("Erreur d'éxecution de la requete :  " + requete);
+                Console.WriteLine("Erreur d'execution de la requete :" + requete);
+                Console.WriteLine(exp.Message);
             }
         }
 
-        public Intervention selectWhereInterventions(int idInter)
+        public Intervention selectWhereIntervention(int idInter)
         {
             Intervention uneIntervention = null;
-            string requete = "select * from intervention where idInter = @idinter ;";
+            string requete = "select * from intervention where  idInter = @idinter ; ";
             try
             {
-                // ouverture de la connexion mysql
+                //ouverture de la connexion Mysql
                 this.maConnexion.Open();
+                //creation de la commande SQL
                 MySqlCommand cmd = this.maConnexion.CreateCommand();
-
                 cmd.CommandText = requete;
                 cmd.Parameters.AddWithValue("@idinter", idInter);
 
-                // parcourir les enregistrement de la table
-
+                //parcourir les enregistrements de la table
                 DbDataReader unReader = cmd.ExecuteReader();
                 try
                 {
                     if (unReader.HasRows)
                     {
-                        while (unReader.Read())
+                        if (unReader.Read())
                         {
                             uneIntervention = new Intervention(
-                                unReader.GetInt32(0), unReader.GetString(1), unReader.GetString(2), unReader.GetString(4), unReader.GetFloat(3),
-                                unReader.GetString(5), unReader.GetString(6)
-                                );
-
-
-
+                               unReader.GetInt32(0), unReader.GetString(1), unReader.GetString(2), unReader.GetString(4), unReader.GetFloat(3),
+                               unReader.GetString(5), unReader.GetString(6)
+                               );
                         }
                     }
                 }
@@ -204,15 +206,15 @@ namespace Orange
                 {
                     Console.WriteLine("Erreur d'extraction des données de la table ");
                 }
-                // fermeture de la connexion
+                //fermeture de la connexion 
                 this.maConnexion.Close();
             }
             catch (Exception exp)
             {
-                Console.WriteLine("Erreur d'éxécution de la requete: " + requete);
+                Console.WriteLine("Erreur d'exécution de la requete :" + requete);
+                Console.WriteLine(exp.Message);
             }
             return uneIntervention;
         }
-
     }
 }
--- Orange/Default.aspx
+++ Orange/Default.aspx
@@ -1,5 +1,6 @@
-﻿<%@page language="C#"%>
-<%@ Import Namespace="System.Collections.Generic"%>
+﻿﻿<%@ Page Language="C#" %>
+<%@  Import Namespace="System.Collections.Generic" %>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,30 +8,76 @@
     <title></title>
 </head>
 <body>
-    <h1>Gestion des interventions</h1>
-<%
-   Orange.Bdd uneBDD = new Orange.Bdd("localhost", "orangeC","root",""); // pas de mdp pour les PC
-%>
+    <h1> Gestion des interventions </h1> 
+        <%
+            Orange.Bdd uneBDD = new Orange.Bdd ("localhost","orangec", "root", "") ; //pas de mdp pour les PC 
 
-<h2>  Liste des interventions </h2>
-<%
+        %>
+          <form method="post" action="">
+              <table border  ="0">
+                  <tr>
+                      <td>Description</td><td> <textarea name="description" rows ="10" cols="100"></textarea></textarea></td>
+                  </tr>
+                   <tr>
+                      <td>Date Intervention</td><td> <input type="date" name="dateinter"></td>
+                  </tr>
+                  <tr>
+                      <td>Statut </td><td> <select  name="statut">
+                          
+                          <option value="programée">Programmée</option>
+                          <option value="terminée">terminée</option>
+                          <option value="en cours">En cours</option>
+                          </select>
+                          </td>
+                  </tr>
+                  <tr>
+                      <td>Montant a payer</td><td> <input type="text" name="montant"></td>
+                  </tr>
+                  <tr>
+                      <td>Technicien</td><td> <input type="text" name="technicien"></td>
+                  </tr>
+                  <tr>
+                      <td>Client</td><td> <input type="text" name="client"></td>
+                  </tr>
+                  <tr>
+                      <td> <input type="reset" name="annuler" value="annuler"></td><td><input type="submit" name="valider" value="valider"></td>
+                  </tr>
 
- string chaine = "<table border = 1> <tr> <td> Id Intervention </td> <td> Description </td> ";
-        chaine += "<td> Date </td> <td> Statut </td> <td> Montant </td> <td> Technicien </td> <td> Client</td> </tr> ";
-
-        List <Orange.Intervention> lesInterventions = uneBDD.selectAllInterventions();
-
-        foreach (Orange.Intervention uneIntervention in lesInterventions)
+              </table>
+          </form>
+    <% 
+        if(Request.Form["valider"] != null)
         {
-        chaine += "<tr> <td>" +uneIntervention.iDinter+"</td> <td>" + uneIntervention.DESCRIPTION
-        + "</td> <td>" + uneIntervention.DATEINTER + "</td> <td>" + uneIntervention.STATUT + "</td> <td>"+ uneIntervention.MONTANT
-        + "</td> <td>" + uneIntervention.TECHNICIEN + "</td> <td>" + uneIntervention.CLIENT +"</td></tr>";
+            string description = Request.Form["description"];
+            string dateinter = Request.Form["dateinter"];
+            string statut = Request.Form["statut"];
+            float montant =float.Parse( Request.Form["montant"]);
+            string technicien = Request.Form["technicien"];
+            string client = Request.Form["client"];
+
+            Orange.Intervention uneIntervention = new Orange.Intervention(description, dateinter, statut, montant, technicien, client);
+
+            uneBDD.insertIntervention(uneIntervention);
+
         }
 
-        chaine+="</table>";
-%>
-        <%=chaine%>
+        %>
 
-
+        <h2> Liste des interventions </h2>
+        <%
+        string chaine = "<table border= 1 > <tr> <td> Id Intervention</td> <td> Description</td> ";
+               chaine += " <td> Date </td><td>Statut </td> <td> Montant </td> <td> Technicien </td> <td> Client </td> </tr> "; 
+        List<Orange.Intervention> lesInterventions = uneBDD.selectAllInterventions (); 
+        
+        foreach (Orange.Intervention uneIntervention in lesInterventions)
+        {
+            chaine += "<tr> <td>"+uneIntervention.iDinter + "</td> <td> "+ uneIntervention.DESCRIPTION 
+            + "</td> <td>" + uneIntervention.DATEINTER +"</td> <td> " + uneIntervention.STATUT +"</td> <td> "+uneIntervention.MONTANT 
+            + "</td> <td>" + uneIntervention.TECHNICIEN +"</td> <td> "+uneIntervention.CLIENT+"</td></tr>";  
+        }
+        chaine += "</table>"; 
+        %>
+        
+        <%= chaine %>
 </body>
 </html>
